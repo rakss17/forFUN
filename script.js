f976624fe3cc56983a3bda171049f095895d7de5
rakss17 <102576549+rakss17@users.noreply.github.com>
2022-08-26 16:25:16 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,184 @@
+var dt = new Date();
+document.getElementById('date').innerHTML=dt;
+function YES(){
+  document.getElementById('yours').style.display = "block";
+  document.getElementById('date').style.display = "block";
+  document.getElementById('kilig').style.display = "block";
+
+  document.getElementById('buttonYES').style.display = "none";
+  document.getElementById('button1').style.display = "none";
+  document.getElementById('main').style.display = "none";
+
+  document.getElementById('button1').style.display = "none";
+  document.getElementById('button2').style.display = "none";
+  document.getElementById('button3').style.display = "none";
+  document.getElementById('button4').style.display = "none";
+  document.getElementById('button5').style.display = "none";
+  document.getElementById('button6').style.display = "none";
+  document.getElementById('button7').style.display = "none";
+  document.getElementById('button8').style.display = "none";
+  document.getElementById('button9').style.display = "none";
+  document.getElementById('button10').style.display = "none";
+
+  document.getElementById('image1').style.display = "none";
+  document.getElementById('image2').style.display = "none";
+  document.getElementById('image3').style.display = "none";
+  document.getElementById('image4').style.display = "none";
+  document.getElementById('image5').style.display = "none";
+  document.getElementById('image6').style.display = "none";
+  document.getElementById('image7').style.display = "none";
+  document.getElementById('image8').style.display = "none";
+  document.getElementById('image9').style.display = "none";
+  document.getElementById('image10').style.display = "none";
+}
+function imageShow1(){
+  document.getElementById('image10')
+  .style.display = "none";
+  document.getElementById('image1').style.display = "block";
+  document.getElementById('image2')
+  .style.display = "none";      
+}
+function imageShow2(){
+  document.getElementById('image2').style.display = "block";
+  document.getElementById('image1')
+  .style.display = "none";      
+}
+function imageShow3(){
+  document.getElementById('image3').style.display = "block";
+  document.getElementById('image2')
+  .style.display = "none";      
+}
+function imageShow4(){
+  document.getElementById('image4').style.display = "block";
+  document.getElementById('image3')
+  .style.display = "none";      
+}
+function imageShow5(){
+  document.getElementById('image5').style.display = "block";
+  document.getElementById('image4')
+  .style.display = "none";      
+}
+function imageShow6(){
+  document.getElementById('image6').style.display = "block";
+  document.getElementById('image5')
+  .style.display = "none";      
+}
+function imageShow7(){
+  document.getElementById('image7').style.display = "block";
+  document.getElementById('image6')
+  .style.display = "none";      
+}
+function imageShow8(){
+  document.getElementById('image8').style.display = "block";
+  document.getElementById('image7')
+  .style.display = "none";      
+}
+function imageShow9(){
+  document.getElementById('image9').style.display = "block";
+  document.getElementById('image8')
+  .style.display = "none";      
+}
+function imageShow10(){
+  document.getElementById('image10').style.display = "block";
+  document.getElementById('image9')
+  .style.display = "none";      
+}
+const button1 = document.getElementById('button1');
+button1.addEventListener('click', () => {
+// 👇️ hide button
+button1.style.display = 'none';
+
+const button2 = document.getElementById('button2');
+button2.style.display = 'block';
+});
+
+const button2 = document.getElementById('button2');
+button2.style.display = 'none';
+button2.addEventListener('click', () => {
+  // 👇️ hide button
+  button2.style.display = 'none';
+
+  const button3 = document.getElementById('button3');
+  button3.style.display = 'block';
+});
+
+const button3 = document.getElementById('button3');
+button3.style.display = 'none';
+button3.addEventListener('click', () => {
+  // 👇️ hide button
+  button3.style.display = 'none';
+
+  const button4 = document.getElementById('button4');
+  button4.style.display = 'block';
+});
+
+const button4 = document.getElementById('button4');
+button4.style.display = 'none';
+button4.addEventListener('click', () => {
+  // 👇️ hide button
+  button4.style.display = 'none';
+
+  const button5 = document.getElementById('button5');
+  button5.style.display = 'block';
+});
+
+
+const button5 = document.getElementById('button5');
+button5.style.display = 'none';
+button5.addEventListener('click', () => {
+  // 👇️ hide button
+  button5.style.display = 'none';
+
+  const button6 = document.getElementById('button6');
+  button6.style.display = 'block';
+});
+
+const button6 = document.getElementById('button6');
+button6.style.display = 'none';
+button6.addEventListener('click', () => {
+  // 👇️ hide button
+  button6.style.display = 'none';
+
+  const button7 = document.getElementById('button7');
+  button7.style.display = 'block';
+});
+
+const button7 = document.getElementById('button7');
+button7.style.display = 'none';
+button7.addEventListener('click', () => {
+  // 👇️ hide button
+  button7.style.display = 'none';
+
+  const button8 = document.getElementById('button8');
+  button8.style.display = 'block';
+});
+
+const button8 = document.getElementById('button8');
+button8.style.display = 'none';
+button8.addEventListener('click', () => {
+  // 👇️ hide button
+  button8.style.display = 'none';
+
+  const button9 = document.getElementById('button9');
+  button9.style.display = 'block';
+});
+
+const button9 = document.getElementById('button9');
+button9.style.display = 'none';
+button9.addEventListener('click', () => {
+  // 👇️ hide button
+  button9.style.display = 'none';
+
+  const button10 = document.getElementById('button10');
+  button10.style.display = 'block';
+});
+
+const button10 = document.getElementById('button10');
+button10.style.display = 'none';
+button10.addEventListener('click', () => {
+  // 👇️ hide button
+  button10.style.display = 'none';
+
+  const button1 = document.getElementById('button1');
+  button1.style.display = 'block';
+});
